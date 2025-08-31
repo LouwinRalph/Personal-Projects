e6530d70cfd9e5f80d3a9d84445925c4378b6dec
LouwinRalph <154980696+LouwinRalph@users.noreply.github.com>
2025-08-31 23:42:42 +0800

    Update Basics_&_Fundamentals_In_Python.py

--- Basics_&_Fundamentals_Python/Basics_&_Fundamentals_In_Python.py
+++ Basics_&_Fundamentals_Python/Basics_&_Fundamentals_In_Python.py
@@ -1,4 +1,4 @@
-#NOT FINISHED, I MIGHT ADD MORE DETAILS TO EACH NUMBER!
+#NOT FINISHED, I MIGHT ADD MORE DETAILS TO EACH NUMBER!!
 
 # 1️⃣ PRINTING
 # ➤ What: Displays output to the console.
@@ -119,6 +119,7 @@ print(squares)
 # ➤ Why: Cleaner than using range(len(...)).
 for index, fruit in enumerate(fruits):
     print(f"{index}: {fruit}")
+
 
 
 
